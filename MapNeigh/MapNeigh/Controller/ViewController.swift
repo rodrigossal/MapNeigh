c2a9130920b7978386f4110c3cfe3b20d451e4c8
Rodrigo Salles Stefani <rodrigo.salless@hotmail.com>
2018-02-23 11:15:17 -0300

    mapa feito

--- MapNeigh/MapNeigh/Controller/ViewController.swift
+++ MapNeigh/MapNeigh/Controller/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import MapKit
+import SwifterSwift
+import UIKit.UIGestureRecognizerSubclass
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    var activeLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 1000
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+
+        let  uilgr = UILongPressGestureRecognizer(target: self, action: #selector(self.addLocationWithLongPress(_:)))
+        
+        
+        
+        map.addGestureRecognizer(uilgr)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        locationManager.requestLocation()
+        map.showsUserLocation = true
+    }
+    
+    @objc func addLocationWithLongPress(_ recognier: UIGestureRecognizer){
+        
+        if recognier.state == .began{
+            let touchPoint = recognier.location(in: map)
+            let coordinates = map.convert(touchPoint, toCoordinateFrom: map)
+            let newLocation = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinates
+            if let location = activeLocation{
+                let distance = location.distance(from: newLocation)
+                annotation.title = "Distância: \(String(format: "%.2f", distance/1000)) KM"
+            }
+            map.addAnnotation(annotation)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        activeLocation = locations[locations.count - 1]
+        let region = MKCoordinateRegionMakeWithDistance((activeLocation?.coordinate)!, 200000, 200000)
+        map.setRegion(region, animated: true)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Deu Ruim")
+    }
+    
+    
 }
 
